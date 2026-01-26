64efd5c44df9753d1083705231f586202934520c
ren yi <1290324602@qq.com>
2026-01-26 22:29:19 +0800

    correct structure

--- structure/components/lib/WB.sv
+++
@@ -1,9 +0,0 @@
-module WB(
-    input logic MemtoRegW,
-    input logic [31:0] ReadDataW, ALUOutW,
-    output logic [31:0] ResultW
-);
-
-assign ResultW = MemtoRegW ? ReadDataW : ALUOutW;
-
-endmodule
